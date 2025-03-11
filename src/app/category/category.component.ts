4bb9c410c4b565b59ea1c227293838aeee722d58
yassir5236 <yassiraitlahmadi@gmail.com>
2025-03-11 10:47:18 +0000

    update the category component style

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -2,62 +2,70 @@ import { Component, OnInit } from '@angular/core';
 import { CategoryService, Category } from '../services/category.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-category',
-  imports: [FormsModule,CommonModule],
-templateUrl: './category.component.html',
-  styleUrl: './category.component.css'
+  imports: [FormsModule, CommonModule],
+  templateUrl: './category.component.html',
+  styleUrl: './category.component.css',
 })
-
-
 export class CategoryComponent implements OnInit {
   categories: Category[] = [];
-  showForm = false; // Pour afficher/masquer le formulaire
-  currentCategory: Partial<Category> = {}; // Catégorie en cours d'édition
+  showForm = false;
+  currentCategory: Partial<Category> = {};
 
-  constructor(private categoryService: CategoryService) {}
+  constructor(
+    private categoryService: CategoryService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
     this.loadCategories();
   }
 
   loadCategories(): void {
-    this.categoryService.getAllCategories().subscribe(data => {
+    this.categoryService.getAllCategories().subscribe((data) => {
       this.categories = data;
     });
   }
 
   openForm(category?: Category): void {
-    this.currentCategory = category ? { ...category } : {}; // Initialiser la catégorie
-    this.showForm = true; // Afficher le formulaire
+    this.currentCategory = category ? { ...category } : {};
+    this.showForm = true;
   }
 
   closeForm(): void {
-    this.showForm = false; // Masquer le formulaire
-    this.currentCategory = {}; // Réinitialiser la catégorie
+    this.showForm = false;
+    this.currentCategory = {};
   }
 
   saveCategory(): void {
     if (this.currentCategory.id) {
-      // Mettre à jour la catégorie existante
-      this.categoryService.updateCategory(this.currentCategory.id, this.currentCategory).subscribe(() => {
-        this.loadCategories();
-        this.closeForm();
-      });
+      this.categoryService
+        .updateCategory(this.currentCategory.id, this.currentCategory)
+        .subscribe(() => {
+          this.loadCategories();
+          this.closeForm();
+          this.toastr.success('Category updated with success!');
+        });
     } else {
-      // Créer une nouvelle catégorie
-      this.categoryService.createCategory(this.currentCategory).subscribe(() => {
-        this.loadCategories();
-        this.closeForm();
-      });
+      this.categoryService
+        .createCategory(this.currentCategory)
+        .subscribe(() => {
+          this.loadCategories();
+          this.closeForm();
+          this.toastr.success('Category created with success!');
+        });
     }
   }
 
   deleteCategory(id: number): void {
     if (confirm('Voulez-vous vraiment supprimer cette catégorie ?')) {
-      this.categoryService.deleteCategory(id).subscribe(() => this.loadCategories());
+      this.categoryService.deleteCategory(id).subscribe(() => {
+        this.loadCategories();
+        this.toastr.success('Category deleted with success!');
+      });
     }
   }
 }
